d7430ee85a6a3d822bc90650a860ff599904fe3c
Kane Menicou <kane.menicou@vivait.co.uk>
2019-08-29 13:34:35 +0100

    [APP-592] Adds job priorities

---
+++ src/Vivait/DelayedEventBundle/Event/PriorityAwareEvent.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vivait\DelayedEventBundle\Event;
+
+interface PriorityAwareEvent
+{
+
+    /**
+     * @return int between 0 and 4294967295. Most urgent: 0, least urgent: 4294967295.
+     */
+    public function getPriority(): int;
+}
